c4e52dc8b78043c60b913a3660fb7d8f9650a6bf
plamenbelev <kasa6teka@gmail.com>
2018-12-09 09:46:25 +0200

    stop-folder

--- express-practise/tests.js
+++ express-practise/tests.js
@@ -1,1 +1,12 @@
-console.log('object');
+let express = require('express');
+let app = express();
+const port = 1337;
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/views/');
+});
+
+app.get('/about', (req, res) => {
+	res.sendFile(__dirname + '/views/about.html');
+});
+app.listen(port);
