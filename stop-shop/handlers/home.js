10ce4fa226dc5d74b6425aabae02e78b43f12942
plamenbelev <kasa6teka@gmail.com>
2018-12-10 16:05:36 +0200

    stage1

--- stop-shop/handlers/home.js
+++ stop-shop/handlers/home.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
+const qs = require('querystring');
 let database = require('../config/database');
 
 
@@ -23,12 +24,31 @@ module.exports = (req, res) => {
 				res.end();
 				return;
 			}
+			let queryData = qs.parse(url.parse(req.url).query);
+
+			let products = database.products.getAll();
+
+			if (queryData.query){
+				products = products.filter(product => 
+					product.name.includes(queryData.query)
+				);
+			}
+			let content = '';
+			for (const product of products) {
+				content +=
+				`<div class="product-cars">
+				  <img class="image-tag" src="${product.image}"
+				  <h2>${product.name}</h2>
+				  <p>${product.description}</p>
+				</div>`;
+			}
+			let html = data.toString().replace('{content}', content);
 
 			res.writeHead(200, {
 				'Content-Type': 'text/html'
 			});
 			
-			res.write(data);
+			res.write(html);
 			res.end();
 			return;
 		});
